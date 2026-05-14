db5ec505f16ce34a4c66e1d167a49e4c532ec352
kokoya7415 <kokoya0526@naver.com>
2026-05-14 20:26:05 +0900

    feat: 프로젝트 초기 설정 및 반영

---
+++ src/main/java/likelion14th/lte/LteApplication.java
@@ -0,0 +1,13 @@
+package likelion14th.lte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion14th/lte/LteApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion14th.lte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
