742df03ae744eb21ffd039e8654067bbdd2b592d
kokoya7415 <kokoya0526@naver.com>
2026-09-16 18:13:21 +0900

    feat: 멋사 피드백 반영 완료

--- src/main/java/likelion14th/lte/statistic/controller/StatisticController.java
+++ src/main/java/likelion14th/lte/statistic/controller/StatisticController.java
@@ -7,9 +7,10 @@ import likelion14th.lte.statistic.dto.response.StatisticResponse;
 import likelion14th.lte.statistic.service.StatisticService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,8 +22,9 @@ public class StatisticController {
     @GetMapping
     @Operation(summary = "통계 조회", description = "유저의 통계 정보를 조회합니다.")
     public ApiResponse<StatisticResponse> getStatistic(
-            @RequestParam Long userId
+            @AuthenticationPrincipal Jwt jwt
     ) {
+        Long userId = Long.valueOf(jwt.getSubject());
         StatisticResponse response = statisticService.getStatistic(userId);
         return ApiResponse.onSuccess(SuccessCode.STATISTICS_GET_SUCCESS, response);
     }
--- src/main/java/likelion14th/lte/todo/controller/TodoController.java
+++ src/main/java/likelion14th/lte/todo/controller/TodoController.java
@@ -14,6 +14,7 @@ import likelion14th.lte.todo.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -31,9 +32,10 @@ public class TodoController {
     @GetMapping
     @Operation(summary = "날짜별 투두 목록 조회", description = "선택한 날짜의 투두 목록을 조회합니다.")
     public ApiResponse<List<TodoListResponse>> getTodosByDate(
-            @RequestParam Long userId,
+            @AuthenticationPrincipal Jwt jwt,
             @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date
     ){
+        Long userId = Long.valueOf(jwt.getSubject());
         List<TodoListResponse> todos = todoService.getTodosByDate(userId, date);
         return ApiResponse.onSuccess(SuccessCode.TODO_LIST_GET_SUCCESS, todos);
     }
@@ -44,11 +46,12 @@ public class TodoController {
     @PostMapping
     @Operation(summary = "투두 생성", description = "일반/루틴 투두를 생성합니다.")
     public ApiResponse<TodoDetailResponse> createTodo(
-            @RequestParam Long userId,
+            @AuthenticationPrincipal Jwt jwt,
             @Valid @RequestBody  TodoCreateRequest todoCreateRequest,
             // 일반 투두는 필수, 루틴 투두는 startDate, endDate 사용
             @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date
             ){
+        Long userId = Long.valueOf(jwt.getSubject());
         TodoDetailResponse createdResponse = todoService.createTodo(userId,todoCreateRequest,date);
         return ApiResponse.onSuccess(SuccessCode.TODO_CREATE_SUCCESS, createdResponse);
     }
@@ -57,10 +60,11 @@ public class TodoController {
     @DeleteMapping("/{todoId}/dates/{date}")
     @Operation(summary = "투두 삭제", description = "선택한 날짜 기준으로 투두를 삭제합니다.")
     public ApiResponse<String> deleteTodo(
-            @RequestParam Long userId,
+            @AuthenticationPrincipal Jwt jwt,
             @PathVariable Long todoId,
             @PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date
     ){
+        Long userId = Long.valueOf(jwt.getSubject());
         todoService.deleteTodo(userId, todoId, date);
         return ApiResponse.onSuccess(SuccessCode.TODO_DELETE_SUCCESS, "OK");
     }
@@ -69,11 +73,12 @@ public class TodoController {
     @PatchMapping("/{todoId}/dates/{date}/complete")
     @Operation(summary = "투두 완료 상태 변경", description = "선택한 날짜의 투두 완료 상태를 변경합니다.")
     public ApiResponse<TodoListResponse> updateTodoComplete(
-            @RequestParam Long userId,
+            @AuthenticationPrincipal Jwt jwt,
             @PathVariable Long todoId,
             @PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date,
             @Valid @RequestBody  TodoCompleteUpdateRequest request
     ) {
+        Long userId = Long.valueOf(jwt.getSubject());
         TodoListResponse updatedResponse = todoService.todoComplete(userId, todoId, date, request.getCompleted());
         return ApiResponse.onSuccess(SuccessCode.TODO_COMPLETE_SUCCESS, updatedResponse);
     }
@@ -82,9 +87,10 @@ public class TodoController {
     @GetMapping("/{todoId}")
     @Operation(summary = "투두 상세 조회", description = "투두의 상세 정보를 조회합니다.")
     public ApiResponse<TodoDetailResponse> getTodoDetail(
-            @RequestParam Long userId,
+            @AuthenticationPrincipal Jwt jwt,
             @PathVariable Long todoId
     ){
+        Long userId = Long.valueOf(jwt.getSubject());
         TodoDetailResponse todo = todoService.getTodoDetail(userId, todoId);
         return ApiResponse.onSuccess(SuccessCode.TODO_DETAIL_GET_SUCCESS, todo);
     }
@@ -93,10 +99,11 @@ public class TodoController {
     @PutMapping("/{todoId}")
     @Operation(summary = "투두 수정", description = "투두의 상세 정보를 수정합니다.")
     public ApiResponse<TodoDetailResponse> updateTodoDetail(
-            @RequestParam Long userId,
+            @AuthenticationPrincipal Jwt jwt,
             @PathVariable Long todoId,
             @Valid @RequestBody TodoUpdateRequest todoUpdateRequest
     ){
+        Long userId = Long.valueOf(jwt.getSubject());
         TodoDetailResponse updatedResponse = todoService.updateTodoDetail(
                 userId,
                 todoId,
